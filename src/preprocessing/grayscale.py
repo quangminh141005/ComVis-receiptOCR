8d8d84982b6546cdaecd7a1e6c0f513bac2c770e
quangminh141005 <minhnq.23bi14288@usth.edu.vn>
2026-02-14 15:38:57 +0700

    add gray scale

---
+++ src/preprocessing/grayscale.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as np
+
+def convert_to_grayscale(image: np.ndarray) -> np.ndarray:
+    if len(image.shape) == 3:
+        return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image
