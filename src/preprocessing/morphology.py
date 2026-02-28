b35bebe97eb86bcc5ec03c523ec41330adcfbcd1
quangminh141005 <minhnq.23bi14288@usth.edu.vn>
2026-02-28 22:28:10 +0700

    add morphology for binary

---
+++ src/preprocessing/morphology.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+DEFAULT_KERNEL_SIZE = (2, 2)
+
+def morphological_cleanup(binary: np.ndarray, kernel_size=DEFAULT_KERNEL_SIZE):
+    kernel = np.ones(kernel_size, np.uint8)
+    closed = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
+    cleaned = cv2.morphologyEx(closed, cv2.MORPH_OPEN, kernel)
+    return cleaned
