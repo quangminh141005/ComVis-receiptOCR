c5e71a40fb773061a7d7943b558c26a4bfd6fe65
quangminh141005 <minhnq.23bi14288@usth.edu.vn>
2026-03-07 10:28:54 +0700

    yes more processing

---
+++ src/preprocessing/dilation.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+DEFAULT_DILATION_KERNEL = (2, 2)
+
+def dilate(binary: np.ndarray, kernel_size=DEFAULT_DILATION_KERNEL, iterations=1) -> np.ndarray:
+    kernel = np.ones(kernel_size, np.uint8)
+    dilated = cv2.dilate(binary, kernel, iterations=iterations)
+    return dilated
