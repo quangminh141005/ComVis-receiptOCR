346b6d96d1bc708eadf5f16e4e85ec9f6ee67e05
quangminh141005 <minhnq.23bi14288@usth.edu.vn>
2026-03-09 00:49:17 +0700

    change kernel size

--- src/preprocessing/morphology.py
+++ src/preprocessing/morphology.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
 
-DEFAULT_KERNEL_SIZE = (3, 3)
+DEFAULT_KERNEL_SIZE = (2,2)
 
-def morphological_cleanup(binary: np.ndarray, kernel_size=(2,2)):
+def morphological_cleanup(binary: np.ndarray, kernel_size=DEFAULT_KERNEL_SIZE):
     kernel = np.ones(kernel_size, np.uint8)
-    opened = cv2.morphologyEx(binary, cv2.MORPH_OPEN, kernel)
-    cleaned = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
-    return cleaned
+    connected = cv2.morphologyEx(binary, cv2.MORPH_CLOSE, kernel)
+    cleaned = cv2.morphologyEx(connected, cv2.MORPH_OPEN, kernel)
+    return connected
 
